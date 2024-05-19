f5bf655ac8e0084863709f733f41fd96e9eb8a22
diegolds <diegolopes087@gmail.com>
2024-05-19 04:58:21 -0300

    Redirecionando para a store

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,32 +1,36 @@
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import ChevronDown from "@modules/common/icons/eye"
 
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-start small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+            className="text-6xl leading-10 text-ui-fg-base font-extrabold  mb-6 text-blue-700"
           >
-            Ecommerce Starter Template
+            Medusa Store
           </Heading>
           <Heading
             level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+            className="text-2xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Powered by Medusa and Next.js
+            a plataforma de vendas online mais utilizada do Brasil
           </Heading>
         </span>
         <a
           href="https://github.com/medusajs/nextjs-starter-medusa"
           target="_blank"
         >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
+          <LocalizedClientLink
+            href="/store"
+            className="text-2xl leading-10 hover:text-blue-600"
+          >
+            ver produtos
+          </LocalizedClientLink>
         </a>
       </div>
     </div>
